89ade41e7913bbf68975668f446ad6c21e39ddf2
REDTECH <rachinthanarajapaksha@gmail.com>
2024-05-29 15:54:00 +0530

    updated

--- src/app/Airport-Transfers/page.tsx
+++ src/app/Airport-Transfers/page.tsx
@@ -1,4 +1,5 @@
 
+import AirportTransferComponent from "@/app/Airport-Transfers/airporttransfercomponent";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navigationBar";
 import ContactSection from "@/components/quickcontact";
@@ -122,29 +123,3 @@ export default function Home() {
   );
 }
 
-interface AirportTransferProps {
-  title: string;
-  description: string;
-  image: string;
-}
-
-export function AirportTransferComponent({
-  title,
-  description,
-  image,
-}: AirportTransferProps) {
-  return (
-    <div className="w-[90vw] lg:w-[15vw] h-auto text-slate-800 border-custom-primaryblue border-2 rounded-xl overflow-hidden shadow-lg mx-auto my-4 flex-1 ">
-      <div className="">
-        <img src={image} alt={title} className="rounded-t-xl scale-75" />
-      </div>
-      <div className="p-4">
-        <h1 className="text-lg font-semibold mb-2 text-custom-primaryblue ">
-          {title}
-        </h1>
-        <p className="text-base font-medium">{description}</p>
-      </div>
-      <div className=" w-full py-2 bg-custom-primaryblue"></div>
-    </div>
-  );
-}
--- src/app/Hire-A-Car/page.tsx
+++ src/app/Hire-A-Car/page.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/footer";
 import Navbar from "@/components/navigationBar";
 import ContactSection from "@/components/quickcontact";
 import ReviewComponent from "@/components/reviewcomponent";
+import VehicleComponent from "@/app/Hire-A-Car/vehiclecomponent";
 import Head from "next/head";
 import Link from "next/link";
 import { IoIosArrowForward } from "react-icons/io";
@@ -110,30 +111,3 @@ export default function Home() {
   );
 }
 
-interface VehicleProps {
-    title: string;
-    imgUrl: string;
-    facts: string[];
-  }
-  
-  export function VehicleComponent({ title, imgUrl, facts }: VehicleProps) {
-    return (
-      <div className=" w-[80vw] mx-auto bg-white shadow-lg rounded-lg overflow-hidden flex">
-        <img src={imgUrl} alt={title} className="w-1/3 h-48 object-cover" />
-        <div className="p-6 flex-1 flex flex-col justify-center">
-          <h2 className="text-xl font-bold mb-2 text-gray-800 text-center">{title}</h2>
-          <ul className="mb-4 space-y-2">
-            {facts.map((fact, index) => (
-              <li key={index} className="text-gray-700">
-                {fact}
-              </li>
-            ))}
-          </ul>
-        </div>
-        
-        <button className="py-2 px-4 bg-custom-primaryblue text-white font-semibold rounded-md shadow-sm hover:bg-custom-secondaryblue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custom-primaryblue self-end">
-            Book This
-          </button>
-      </div>
-    );
-  }
