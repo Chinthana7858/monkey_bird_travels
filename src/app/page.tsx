c39866b3f5feaa3a2dbb0ab0b4dc00886104a434
Chinthana Prabhashitha <prabhashitharac.20@uom.lk>
2024-12-12 14:35:13 +0530

    bug_fix5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Review from "@/components/review";
 import ReviewComponent from "@/components/reviewcomponent";
 import Head from "next/head";
 import TourPackage from "./Tour-Packages/tourpackageCard";
+import { tourPackages } from "@/database/tourpackages";
 
 export default function Home() {
   return (
@@ -71,9 +72,9 @@ export default function Home() {
             Our most popular Tour Packages
           </div>
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 w-full lg:w-[80vw] mx-auto p-5">
-            <TourPackage id={"1"} key={1}/>
-            <TourPackage id={"2"} key={2}/>
-            <TourPackage id={"3"} key={3}/>
+          {tourPackages.map((packageInfo) => (
+    <TourPackage key={packageInfo.id} id={packageInfo.id} />
+  ))}
           </div>
           <div className=" text-3xl lg:px-16 px-5 font-semibold text-white text-center p-3 bg-red-800 rounded-lg m-4">
             Popular destinations
